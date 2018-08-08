4f0ca86e4f50cf224214cd77b9d3122488ac03b2
epord <pedro.balaguer@hotmail.com>
2018-08-07 22:00:02 -0300

    Basic player

---
+++ Assets/Scripts/ControllerComponent.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ControllerComponent : MonoBehaviour {
+    
+    public Vector2 movement = new Vector2(0, 0);
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetKey(KeyCode.LeftArrow)) {
+            movement.Set(-1, 0);
+        } else if (Input.GetKey(KeyCode.RightArrow)) {
+            movement.Set(1, 0); 
+        } else {
+            movement.Set(0, 0);
+        }
+	}
+}
