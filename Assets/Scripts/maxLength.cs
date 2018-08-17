7c0bef008cb682f66782b072bfe423dca99711bd
HubertHamelin <32563790+HubertHamelin@users.noreply.github.com>
2018-08-17 16:46:11 -0300

    Graphics tests and new blocks
    First tests on a more graphic solution  in a new scene (with our code behind, I just imported some sprites of the original game).
    New blocks are the one to resize the player (bigger and smaller).

--- Assets/Scripts/maxLength.cs
+++ Assets/Scripts/maxLength.cs
@@ -2,42 +2,40 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Sticky : MonoBehaviour {
+public class maxLength : MonoBehaviour {
 
-    private GameObject ball;
-    private Vector2 temp;
-    public float timer = 10;
+    private GameObject player;
     private bool active;
+    public float timer = 10;
 
-    private void Start()
-    {
-        ball = GameObject.FindGameObjectWithTag("Ball");
-    }
-
-    public void Update()
-    {
-        if (active) {
+	// Use this for initialization
+	void Start () {
+        player = GameObject.FindGameObjectWithTag("Player");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (active)
+        {
             timer = Mathf.Max(timer - Time.deltaTime, 0);
             if (timer == 0)
             {
-                var ballMove = ball.GetComponent<Ball_move>();
-                ballMove.NoStickMode();
+                player.GetComponent<mover>().unSetLongMode();
 
                 Destroy(gameObject);
             }
         }
-    }
+	}
 
-    void OnCollisionEnter2D(Collision2D collisionInfo)
+    private void OnCollisionEnter2D(Collision2D collisionInfo)
     {
         if (collisionInfo.gameObject.name == "player")
         {
             var falling = GetComponent<FallingPowerUp>();
             Destroy(falling);
             active = true;
-            ball.GetComponent<Ball_move>().SetStickMode();
+            player.GetComponent<mover>().setLongMode();
             this.transform.position = new Vector2(-10000, 0);
-            //gameObject.SetActive(false);
         }
     }
 }
--- Assets/Scripts/mover.cs
+++ Assets/Scripts/mover.cs
@@ -1,15 +1,27 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class mover : MonoBehaviour {
 
     public float speed = 150;
     public GameObject projectile1;
+    public bool longMode;
+    public bool shortMode;
+    private Image m_Image;
+    private BoxCollider2D m_BoxCollider2D;
+    private Transform m_Transform;
+    public Sprite normalSprite;
+    public Sprite longSprite;
+    public Sprite shortSprite;
+    private float const_modifyLength = 0.5f;
 
     // Use this for initialization
     void Start () {
-		
+        m_Image = GetComponent<Image>();
+        m_Transform = GetComponent<Transform>();
+        m_BoxCollider2D = GetComponent<BoxCollider2D>();
 	}
 
     //// Update is called once per frame
@@ -42,15 +54,34 @@ public class mover : MonoBehaviour {
             Destroy(bullet, 2);
         }
 
-        // Get Horizontal Input
+        // Direction and speed
         float h = Input.GetAxisRaw("Horizontal");
-
-        // Set Velocity (movement direction * speed)
         GetComponent<Rigidbody2D>().velocity = Vector2.right * h * speed;
     }
 
-    void FixedUpdate()
+    public void setLongMode()
     {
-        
+        longMode = true;
+        //Update image and size of both transform and Boxcollider
+        m_Image.sprite = longSprite;
+        var temp = m_Transform.localScale;
+        temp.x += const_modifyLength;
+        m_Transform.localScale = temp;
+        temp = m_BoxCollider2D.size;
+        temp.x += const_modifyLength;
+        m_BoxCollider2D.size = temp;
+    }
+
+    public void unSetLongMode()
+    {
+        longMode = false;
+        //Update image and size of both transform and Boxcollider
+        m_Image.sprite = shortSprite;
+        var temp = m_Transform.localScale;
+        temp.x -= const_modifyLength;
+        m_Transform.localScale = temp;
+        temp = m_BoxCollider2D.size;
+        temp.x -= const_modifyLength;
+        m_BoxCollider2D.size = temp;
     }
 }
