ee38c2a7265178107af988299b175b41d9fa2a31
HubertHamelin <32563790+HubertHamelin@users.noreply.github.com>
2018-08-22 21:02:25 -0300

    Basic Menu added
    Basic menu with music and direct transition to the playing scene (handled with the GameManager)

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -8,46 +8,68 @@ public class GameManager : MonoBehaviour {
     public GameObject continueSprite;
     public SoundManager soundManager;
 
+    private bool isPlaying;
     private bool isGameOver = false;
     private float flickeringTime = 0.3f;
     private float timer = 0.0f;
     private const float EPSILON = 0.001f;
+    private string playingScene = "Graphic_test_scene";
+    private string menuScene = "MenuScene";
 
 
     void Start()
     {
-        //Instantiate(soundManager);
+        if (SceneManager.GetActiveScene().name == playingScene)
+        {
+            isPlaying = true;
+        }
+        else if (SceneManager.GetActiveScene().name == menuScene)
+        {
+            isPlaying = false;
+        }
         Time.timeScale = 1;
         gameOverSprite.GetComponent<Renderer>().enabled = false;
         continueSprite.GetComponent<Renderer>().enabled = false;
     }
 	
-	// Update is called once per frame
     void Update () {
-        if (isGameOver)
+        if (isPlaying == false)
         {
             if (Input.anyKeyDown)
             {
-                SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+                SceneManager.LoadScene(playingScene);
+                isPlaying = true;
             }
-            timer = Mathf.Min(timer + Time.deltaTime, 0.3f);
-            if (timer < 0.2f) {
-                timer = 0.0f;
-                continueSprite.GetComponent<Renderer>().enabled = !continueSprite.GetComponent<Renderer>().enabled;
-            }
+        }
+        else
+        {
+            if (isGameOver)
+            {
+                if (Input.anyKeyDown)
+                {
+                    SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+                }
+                timer = Mathf.Min(timer + Time.deltaTime, 0.3f);
+                if (timer < 0.2f)
+                {
+                    timer = 0.0f;
+                    continueSprite.GetComponent<Renderer>().enabled = !continueSprite.GetComponent<Renderer>().enabled;
+                }
 
+            }
+            if (GameObject.FindGameObjectsWithTag("Ball").Length == 0)
+            {
+                // GAME OVER
+                gameOverSprite.GetComponent<Renderer>().enabled = true;
+                continueSprite.GetComponent<Renderer>().enabled = true;
+                Time.timeScale = 0;
+                isGameOver = true;
+            }
+            if (GameObject.FindGameObjectsWithTag("Brick").Length == 0)
+            {
+                // WIN
+            }
         }
-        if (GameObject.FindGameObjectsWithTag("Ball").Length == 0)
-		{
-            // GAME OVER
-            gameOverSprite.GetComponent<Renderer>().enabled = true;
-            continueSprite.GetComponent<Renderer>().enabled = true;
-            Time.timeScale = 0;
-            isGameOver = true;
-        }
-		if (GameObject.FindGameObjectsWithTag("Brick").Length == 0)
-		{
-			// WIN
-		}
-	}
+    }
+        
 }
