2c2a168a65d1dcb6e7e721e20fdfa2ce496a450c
epord <pedro.balaguer@hotmail.com>
2018-08-28 20:25:35 -0300

    Corrected PowerUpManager

---
+++ Assets/Scripts/LifeManager.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LifeManager : MonoBehaviour {
+    public int lifesRemaining = 3;
+
+    void Awake()
+    {
+        // Do not destroy this game object:
+        DontDestroyOnLoad(this);
+    }
+
+    int getLifesRemaining()
+    {
+        return lifesRemaining;
+    }
+}
